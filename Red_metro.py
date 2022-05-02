b1fe76c92415f75b7c59b30bbad9aa7381ddde78
jicaceress2703 <newphysicslife@gmail.com>
2022-05-02 08:02:38 -0500

    deifinicion de clases

--- Red_metro.py
+++ Red_metro.py
@@ -1,12 +1,46 @@
 import csv
+from optparse import Values
+def ruta(origen,destino,red_trans,**kwarg):
+    red_trans=red()
+    origen_aux=origen
+    count=red_trans.estaciones().find(origen_aux)
+    rutas=[]
+    for i in red_trans.estacionAd()[count]:
+        
 
-def stations_info(archive):
-    stations=list()
-    with open(archive) as red_db:
-        reader=csv.reader(red_db)
-        next(reader)
-        for row in reader:
-            stations.append(row)
-    return stations
-   
-print(stations_info('red.csv'))
+
+class red:
+    def __init__(self,archivo):
+        self.archivo=archivo
+        self.puntos=[]
+        self.colores=[]
+        self.adyacentes=[]
+    
+    def estaciones(self):
+        for lista in self.archivo:
+            self.puntos.append(lista[0])
+        return self.puntos
+
+    def colorEstacion(self):
+        for lista in self.archivo:
+            self.colores.append(lista[1])
+        return self.colores
+
+    def estacionAd(self):
+        for lista in self.archivo:
+            self.adyacentes.append(lista[2])
+        return self.adyacentes
+    
+
+red_transporte=list()
+
+with open('red.csv') as red_tren:
+    reader= csv.reader(red_tren)
+    next(reader)
+    for rows in reader:
+        red_transporte.append(rows)
+
+
+
+
+
